3071fc002c5552055a3816246ece11f362ad3336
officialsachin <sachin@litschool.in>
2026-01-23 10:00:18 +0530

    added DTO

---
+++ src/main/java/com/sachin/ood/parkinglot/controllers/ParkingLotController.java
@@ -0,0 +1,10 @@
+package com.sachin.ood.parkinglot.controllers;
+
+import com.sachin.ood.parkinglot.dtos.CreateParkingLotRequestDto;
+
+public class ParkingLotController {
+
+    createParkingLot(CreateParkingLotRequestDto request) ;
+
+}
+// DTO --> Data Transfer Object, for every request from the client, for every response to the client--> we create DTO
---
+++ src/main/java/com/sachin/ood/parkinglot/dtos/CreateParkingLotRequestDto.java
@@ -0,0 +1,7 @@
+package com.sachin.ood.parkinglot.dtos;
+
+public class CreateParkingLotRequestDto {
+
+    String address;
+    int numberOfFloors;
+}
